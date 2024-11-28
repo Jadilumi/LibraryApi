5a5b79fa1bac4986cdf15399a89d6b88da940dba
Dielder Leal <86753778+Dielder@users.noreply.github.com>
2024-11-27 22:26:24 -0300

    Update CorsConfig.java

--- libraryapi/src/main/java/com/jadilumi/library/config/cors/CorsConfig.java
+++ libraryapi/src/main/java/com/jadilumi/library/config/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedHeaders("*")
+                .allowedHeaders("Content-Type", "Authorization")
                 .allowedOrigins("https://libraryweb.vercel.app")
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .allowCredentials(true);
